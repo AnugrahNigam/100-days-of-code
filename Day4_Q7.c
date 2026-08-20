82f715e32e8d5ba9bed6163f7a4378c3bac46567
AnugrahNigam <anugrah.40918@stu.upes.ac.in>
2026-08-20 11:09:10 +0530

    questions

---
+++ Day4_Q7.c
@@ -0,0 +1,16 @@
+/*Q7: Write a program to swap two numbers without using a third variable.*/
+
+#include <stdio.h>
+int main()
+{ int a,b;
+    printf("enter the values of two numbers");
+    scanf("%d %d", &a, &b);
+
+    a = a+b;
+    b = a-b;
+    a = a-b;
+
+    printf("the swapped values are= %d %d", a, b);
+     return 0;
+
+}
---
+++ Day5_Q10.c
@@ -0,0 +1,18 @@
+//Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+#include <stdio.h>
+
+int main() {
+    int ts, h,m, s;
+
+    printf("Enter time in seconds: ");
+    scanf("%d", &ts);
+
+    h = ts   / 3600;
+    m = (ts % 3600) / 60;
+    s = ts % 60;
+
+    printf("Time in hours:minutes:seconds format: %d:%d:%d\n", h, m, s);
+
+    return 0;
+}
